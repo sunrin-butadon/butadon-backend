6f860e3c483e4e04ec6dd03b561ea078efcbec1c
박찬규 <126973231+pck-llight@users.noreply.github.com>
2025-07-19 10:43:13 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://org-graduated-clients-motels.trycloudflare.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
